aaa47bbec5a05530b594b43e4f3bc1b917d9d8c1
kylepinheiro <kylepinheiro@users.noreply.github.com>
2016-04-25 00:12:07 -0400

    Create forms.py

---
+++ checkout/forms.py
@@ -0,0 +1,15 @@
+from django.forms import *
+
+from .models import User
+
+
+class OtherCheckoutForm(forms.Form):
+    user_id = ModelChoiceField(queryset=User.objects.all().order_by('first_name'))
+
+
+class CheckoutForm(ModelForm):
+    item_id = CharField()
+
+    class Meta:
+        model = User
+        fields = ['user_id', 'first_name', 'last_name', 'email']
